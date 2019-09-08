3c835f3969963075a61058074fb5edf4158a5766
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-08 16:03:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,8 +17,6 @@ class Scraper
       students[student.to_sym] = {
         :name => student.css(".student-card").first.css("h4.student-name").text.strip,
         :location => student.css(".student-card").first.css("p.student-location").text.strip,
-        binding.pry
-
         :profile_url => student.css(".student-card").first.css("a").attribute("href").text.strip
 
       }
