5baa93573c946849a172b73bede7cfa0a550766b
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-08 16:50:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,8 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student|
-      student = Student.new(student)
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
