7b4ce5f73319b1270c570b9eca8d8abda112cd4d
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-08 15:54:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     student = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/"))
+    #index_url.css(".student-card")
     #name: student.css(".student-card").first.css("h4.student-name").text.strip
     #location: student.css(".student-card").first.css("p.student-location").text.strip
     #profile_url: student.css(".student-card").first.css("a").attribute("href").text.strip
