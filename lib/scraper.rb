de5bb5482d564515e784fba5cdadb17dea8bbcf5
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-08 16:37:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,25 +23,46 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    profile = Nokogiri::HTML(open(profile_url))
-    person = {}
+  #   profile = Nokogiri::HTML(open(profile_url))
+  #   person = {}
+  #
+  #   icons = profile.css.(".social-icon-container a").collect{|icon| icon.attribute("href").value}
+  #   icons.each do |link|
+  #     if link.include?("twitter")
+  #       person[:twitter] = link
+  #     elsif link.include?("linkedin")
+  #       person[:linkedin] = link
+  #     elsif link.include?("github")
+  #       person[:github] = link
+  #     elsif link.include?(".com")
+  #       person[:blog] = link
+  #     end
+  #   end
+  # #  binding.pry
+  #   person[:profile_quote] = profile.css(".profile-quote").text
+  #   person[:bio] = profile.css("div.description-holder p").text
+  #   person
 
-    icons = profile.css.(".social-icon-container a").collect{|icon| icon.attribute("href").value}
-    icons.each do |link|
-      if link.include?("twitter")
-        person[:twitter] = link
-      elsif link.include?("linkedin")
-        person[:linkedin] = link
-      elsif link.include?("github")
-        person[:github] = link
-      elsif link.include?(".com")
-        person[:blog] = link
+  page = Nokogiri::HTML(open(profile_url))
+      student = {}
+
+      # student[:profile_quote] = page.css(".profile-quote")
+      # student[:bio] = page.css("div.description-holder p")
+      container = page.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
+      container.each do |link|
+        if link.include?("twitter")
+          student[:twitter] = link
+        elsif link.include?("linkedin")
+          student[:linkedin] = link
+        elsif link.include?("github")
+          student[:github] = link
+        elsif link.include?(".com")
+          student[:blog] = link
+        end
       end
-    end
-  #  binding.pry
-    person[:profile_quote] = profile.css(".profile-quote").text
-    person[:bio] = profile.css("div.description-holder p").text
-    person
+      student[:profile_quote] = page.css(".profile-quote").text
+      student[:bio] = page.css("div.description-holder p").text
+      student
   end
 
 end
