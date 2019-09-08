5a768d8c1bb39d072f311aa39c35ef99a8d5b34e
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-08 16:07:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,14 +5,14 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    student = Nokogiri::HTML(open(index_url))
+    page = Nokogiri::HTML(open(index_url))
     #index_url.css(".student-card")
     #name: student.css(".student-card").first.css("h4.student-name").text.strip
     #location: student.css(".student-card").first.css("p.student-location").text.strip
     #profile_url: student.css(".student-card").first.css("a").attribute("href").text.strip
     students = []
     #binding.pry
-    student.css(".student-card").each do |student|
+    page.css(".student-card").each do |student|
       students[student.to_sym] = {
         :name => student.css(".student-card").first.css("h4.student-name").text.strip,
         :location => student.css(".student-card").first.css("p.student-location").text.strip,
