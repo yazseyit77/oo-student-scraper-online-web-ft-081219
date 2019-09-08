1db29fec4e1c9d4fcab351e4551e273dcf37eadc
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-08 16:04:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,9 +14,9 @@ class Scraper
     students = {}
     #binding.pry
     index_url.css(".student-card").each do |student|
-      students[student.to_sym] = {
-        binding.pry
+      binding.pry
 
+      students[student.to_sym] = {
         :name => student.css(".student-card").first.css("h4.student-name").text.strip,
         :location => student.css(".student-card").first.css("p.student-location").text.strip,
         :profile_url => student.css(".student-card").first.css("a").attribute("href").text.strip
